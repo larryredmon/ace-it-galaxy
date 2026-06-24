7c1dfdc4859fb462ff9c91937234e6dd7500b6ac
Larry Redmon <larry@aceitgalaxy.com>
2026-06-24 21:48:03 +0000

    Revert - restore working config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash]-v2.js`,
-        chunkFileNames: `assets/[name]-[hash]-v2.js`,
-        assetFileNames: `assets/[name]-[hash]-v2.[ext]`
-      }
-    }
-  }
 })
